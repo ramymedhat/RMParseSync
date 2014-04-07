85a165b0e96031921dfe476dd269daf7dafb7e6a
ramymedhat <me@ramymedhat.com>
2014-04-06 20:55:57 -0400

    Initial Commit

---
+++ ParseSync/ParseSync.h
@@ -0,0 +1,13 @@
+//
+//  ParseSync.h
+//  ParseSync
+//
+//  Created by Ramy Medhat on 2014-04-06.
+//  Copyright (c) 2014 Inovaton. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ParseSync : NSObject
+
+@end
---
+++ ParseSync/ParseSync.m
@@ -0,0 +1,13 @@
+//
+//  ParseSync.m
+//  ParseSync
+//
+//  Created by Ramy Medhat on 2014-04-06.
+//  Copyright (c) 2014 Inovaton. All rights reserved.
+//
+
+#import "ParseSync.h"
+
+@implementation ParseSync
+
+@end
